149a87278d5b15b0190317750f884d695eb37c64
Javikh33 <72210148+Javikh33@users.noreply.github.com>
2025-10-24 19:38:28 +0200

    Scripts
    Carpeta de scripts más script de comandos a modificar

---
+++ videojuego/Scripts/comandos.gd
@@ -0,0 +1,12 @@
+extends Node
+
+const COMANDOS = {
+	"comando1": {
+		"displayname": "barba",
+		"details": "Una barba que gira sin parar atacará a tus enemigos",
+		"level": "Nivel: 1",
+		"prerequisite": [],
+		"type": "weapon"
+		
+		}
+	}
