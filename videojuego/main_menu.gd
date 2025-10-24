f74a162b74b98e96b338953f1566048790310161
Azzazel <mastermetal7@gmail.com>
2025-10-24 22:35:29 +0200

    MUSIQUITA JIJI

--- videojuego/main_menu.gd
+++ videojuego/main_menu.gd
@@ -5,10 +5,19 @@ extends Control
 @onready var creditos: Button = $ColorRect/GridContainer/Creditos
 @onready var abortar_mision: Button = $ColorRect/GridContainer/AbortarMision
 
+@onready var audio_fx: AudioStreamPlayer2D = $AudioFX
+@onready var audio_ambient: AudioStreamPlayer2D = $AudioAmbient
+
+var playback:AudioStreamPlaybackPolyphonic
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	audio_fx.stream = load("res://Assets/FX/HoverMenu.mp3")
+	audio_ambient.play()
+	pass
+
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,10 +26,26 @@ func _process(delta: float) -> void:
 
 
 func _on_lanzar_rover_pressed() -> void:
+	audio_fx.play()
 	get_tree().change_scene_to_file("res://Escenas/Escenario_interfaz.tscn")
-
-
 
 
 func _on_abortar_mision_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_lanzar_rover_mouse_entered() -> void:
+	audio_fx.play()
+
+
+
+func _on_configuracion_mouse_entered() -> void:
+	audio_fx.play()
+
+
+func _on_creditos_mouse_entered() -> void:
+	audio_fx.play()
+
+
+func _on_abortar_mision_mouse_entered() -> void:
+	audio_fx.play()
