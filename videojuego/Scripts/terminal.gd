06af82a441e26e757b7301b2b88587d731ed35fa
Azzazel <mastermetal7@gmail.com>
2025-10-25 00:45:23 +0200

    FX
    Soniditos y musiquita

--- videojuego/Scripts/terminal.gd
+++ videojuego/Scripts/terminal.gd
@@ -1,5 +1,9 @@
 extends Node2D
+@onready var ambient_music: AudioStreamPlayer2D = $AmbientMusic
+@onready var typing_fx: AudioStreamPlayer2D = $TypingFX
+@onready var submit_fx: AudioStreamPlayer2D = $SubmitFX
 
+@onready var intro_console_fx: AudioStreamPlayer2D = $IntroConsoleFX
 @onready var terminal: LineEdit = $TerminalComandos
 @onready var infoComandos: TextEdit =$InformacionComandos
 signal señalControl(String)
@@ -24,10 +28,19 @@ func _texto_pa_comandos(texto: String) -> void:
 	
 	
 func _process(delta: float) -> void:
+
 	pass
+	
 
 
 func _on_terminal_comandos_text_submitted(comando: String) -> void:
 	#terminal.text_submitted.connect(_texto_pa_comandos)
 	emit_signal("señalControl", comando)
 	terminal.clear()
+
+
+func _on_terminal_comandos_text_changed(new_text: String) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		submit_fx.play()
+	else:
+		typing_fx.play()
