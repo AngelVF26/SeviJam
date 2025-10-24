e3837b7f78caa2b9e768200352ef5dac3f8c67f7
Azzazel <mastermetal7@gmail.com>
2025-10-24 20:28:16 +0200

    GdScriptMainMenu
    Creacion GDSCript para main menu y todos los assets

---
+++ videojuego/Escenas/main_menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+@onready var lanzar_rover: Button = $ColorRect/GridContainer/LanzarRover
+@onready var configuracion: Button = $ColorRect/GridContainer/Configuracion
+@onready var creditos: Button = $ColorRect/GridContainer/Creditos
+@onready var abortar_mision: Button = $ColorRect/GridContainer/AbortarMision
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
