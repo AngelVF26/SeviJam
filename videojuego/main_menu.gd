094b1ec16f9dab4b362cbb0e64ca9be544e58e27
Azzazel <mastermetal7@gmail.com>
2025-10-25 02:10:01 +0200

    SOUND MANAGMENT NO VA

--- videojuego/main_menu.gd
+++ videojuego/main_menu.gd
@@ -7,11 +7,23 @@ extends Control
 
 @onready var audio_fx: AudioStreamPlayer2D = $AudioFX
 @onready var audio_ambient: AudioStreamPlayer2D = $AudioAmbient
+@onready var grid_opciones: Control = $ColorRect/MenuOpciones
 
+@onready var grid_main: GridContainer = $ColorRect/GridMain
+@onready var sfx_volume: HScrollBar = $ColorRect/MenuOpciones/FXBar
+@onready var music_volume: HScrollBar = $ColorRect/MenuOpciones/MusicaBar
 
+@onready var music_index = AudioServer.get_bus_index("Music")
+@onready var sfx_index = AudioServer.get_bus_index("Sound Effects")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	AudioGlobal.music_volume = music_volume.value
+	AudioGlobal.sfx_volume = sfx_volume.value
+	AudioServer.set_bus_volume_db(music_index,AudioGlobal.music_volume)
+	AudioServer.set_bus_volume_db(sfx_index,AudioGlobal.sfx_volume)
+	grid_opciones.visible = false
+	grid_main.visible = true
 	audio_fx.stream = load("res://Assets/FX/HoverMenu.mp3")
 	audio_ambient.play()
 	pass
@@ -22,16 +34,13 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	volume_update()
-	
+	pass
 	
 
 func volume_update():
-	var music_volume = $ColorRect/MenuOpciones/MusicaBar
-	var sfx_volume = $ColorRect/MenuOpciones/FXBar
+
 	AudioGlobal.music_volume = music_volume.value
 	AudioGlobal.sfx_volume = sfx_volume.value
-	var music_index = AudioServer.get_bus_index("Music")
-	var sfx_index = AudioServer.get_bus_index("Sound Effects")
 	AudioServer.set_bus_volume_db(music_index,AudioGlobal.music_volume)
 	AudioServer.set_bus_volume_db(sfx_index,AudioGlobal.sfx_volume)
 
@@ -60,3 +69,13 @@ func _on_creditos_mouse_entered() -> void:
 
 func _on_abortar_mision_mouse_entered() -> void:
 	audio_fx.play()
+
+
+func _on_configuracion_pressed() -> void:
+	grid_main.visible = false
+	grid_opciones.visible = true
+
+
+func _on_aceptar_config_pressed() -> void:
+	grid_main.visible = true
+	grid_opciones.visible = false
