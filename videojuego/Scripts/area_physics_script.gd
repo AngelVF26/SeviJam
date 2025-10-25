aed4e027704df08a89d279e05bbf5d920353368b
Psycody <jaimefl123@gmail.com>
2025-10-25 12:26:10 +0200

    player things

--- videojuego/Scripts/area_physics_script.gd
+++ videojuego/Scripts/area_physics_script.gd
@@ -1,8 +1,16 @@
 extends Area2D
 
-
+@export var obstaculo:StaticBody2D
 @export var imagen:CompressedTexture2D
+@export var is_wall:bool = false
 
+
+func _ready() -> void:
+	(obstaculo.get_child(0) as CollisionShape2D).shape = $CollisionShape2D.shape
+	if is_wall:
+		obstaculo.process_mode = Node.PROCESS_MODE_INHERIT
+	else:
+		obstaculo.process_mode = Node.PROCESS_MODE_DISABLED
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is physics_player:
