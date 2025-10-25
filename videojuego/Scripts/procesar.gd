6646aae0b2fedae63b4fcd4c5d2020abef6a9ef0
Servelgar <svelazquezg96@gmail.com>
2025-10-25 11:17:25 +0200

    señales 4 el regreso

--- videojuego/Scripts/procesar.gd
+++ videojuego/Scripts/procesar.gd
@@ -10,3 +10,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_comand_controller_procesar_señal() -> void:
+	pass # Replace with function body.
